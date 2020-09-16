574308f016670ae422b387ddb0b68dabef0334fc
Tania Nardandrea-Isakovic <rtnrtn@gmail.com>
2020-09-16 22:13:12 +0000

    Added admin dashboard counts and links.

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,5 +3,8 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: Rails.configuration.http_basic_auth[:name], password: Rails.configuration.http_basic_auth[:password]
   
   def show
+    @product_count = Product.count()
+    @category_count = Category.count()
+    @categories = Category.all
   end
 end
