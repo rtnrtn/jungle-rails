8b9f82aef812e0610bc3e86c0bb9ee6a2d53b12b
Tania Nardandrea-Isakovic <rtnrtn@gmail.com>
2020-09-17 01:13:18 +0000

    Created admin page for Categories.

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :categories, only: [:create, :new, :index]
   end
 
   get 'about' => 'about#show'
